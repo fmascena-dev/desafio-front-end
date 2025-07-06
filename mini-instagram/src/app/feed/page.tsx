a896a82e53a899ef738f62f7bafbb8894e4c6cc6
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-07-06 17:06:34 -0300

    feat: create Feed page and integrate Header component

---
+++ mini-instagram/src/app/feed/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import Header from "@/components/Header";
+
+export default function Feed() {
+    return (
+        <main className="w-full max-w-6xl px-4 py-6 mx-auto overflow-x-hidden">
+            <Header />
+        </main>
+    )
+}
