b3ca93f9b1cbfe1db31db2adc0305e57b5d8bfb5
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-07-06 20:09:25 -0300

    feat: implement feed page with image fetching, search integration, and photo display

--- mini-instagram/src/app/feed/page.tsx
+++ mini-instagram/src/app/feed/page.tsx
@@ -1,12 +1,54 @@
-"use client"
+"use client";
+
+import { useEffect, useState } from "react";
+import { Photo } from "@/types/photo";
+import { fetchPhotos } from "@/utils/unsplash";
 import Header from "@/components/Header";
 import SearchBar from "@/components/SearchBar";
+import PhotoCard from "@/components/PhotoCard";
+import { useFavoritesInit } from "@/store/useFavorites";
 
 export default function Feed() {
-    return (
-        <main className="w-full max-w-6xl px-4 py-6 mx-auto overflow-x-hidden">
-            <Header />
-            <SearchBar />
-        </main>
-    )
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    loadPhotos();
+  }, []);
+
+  const loadPhotos = async (searchTerm?: string) => {
+    try {
+      setLoading(true);
+      const data = await fetchPhotos(searchTerm);
+      setPhotos(data);
+    } catch (error) {
+      console.error("Erro ao carregar fotos!", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSearch = (searchTerm: string) => {
+    loadPhotos(searchTerm);
+  };
+
+  useFavoritesInit();
+
+  return (
+    <main className="w-full max-w-6xl px-4 py-6 mx-auto overflow-x-hidden">
+      <Header />
+      <SearchBar onSearch={handleSearch} />
+      {loading ? (
+        <p className="text-gray-200 font-bold text-2xl flex justify-center items-center mt-6">
+          Carregando imagens...
+        </p>
+      ) : (
+        <div className="grid gap-6 grid-cols-1 sm:gri-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-6">
+          {photos.map((photo) => (
+            <PhotoCard key={photo.id} photo={photo} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
 }
