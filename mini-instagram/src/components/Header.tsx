1d258bda46f3ba5006d842dfc632ab6a001e3a68
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-07-06 17:27:15 -0300

    feat: enhancing the header with active link styling, logout functionality and authentication status

--- mini-instagram/src/components/Header.tsx
+++ mini-instagram/src/components/Header.tsx
@@ -1,45 +1,89 @@
-"use client"
+"use client";
 import { Lobster } from "next/font/google";
 import { TypingAnimation } from "./magicui/typing-animation";
 import { cn } from "@/lib/utils";
-// import Link from "next/link";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const lobster = Lobster({ weight: "400", subsets: ["latin"] });
 
 export default function Header() {
+  const pathname = usePathname();
+  const router = useRouter();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const logged = localStorage.getItem("auth");
+    setIsLoggedIn(!!logged);
+  }, []);
 
+  const handleLogout = () => {
+    localStorage.removeItem("auth");
+    router.push("/login");
+  };
 
-
-    return (
-      <header className="flex flex-col justify-between items-center mb-6 md:flex-row lg:flex-row">
-        <h1
-          className={`${lobster.className} text-2xl font-bold text-[#00ffd5] h-20`}
+  return (
+    <header className="flex flex-col justify-between items-center mb-6 md:flex-row lg:flex-row">
+      <h1
+        className={`${lobster.className} text-2xl font-bold text-[#00ffd5] h-20`}
+      >
+        <TypingAnimation
+          className={cn(
+            "text-4xl font-bold leading-[5rem] tracking-[-0.02em]",
+            "bg-gradient-to-r from-[#f09433] via-[#dc2743] to-[#bc1888]",
+            "bg-[length:200%_200%] bg-clip-text text-transparent animate-gradient"
+          )}
         >
-          <TypingAnimation
-            className={cn(
-              "text-4xl font-bold leading-[5rem] tracking-[-0.02em]",
-              "bg-gradient-to-r from-[#f09433] via-[#dc2743] to-[#bc1888]",
-              "bg-[length:200%_200%] bg-clip-text text-transparent animate-gradient"
-            )}
+          InstaPocket
+        </TypingAnimation>
+      </h1>
+
+      <nav className="flex items-center gap-4 text-sm font-medium px-2 space-x-8">
+        <Link
+          href="/feed"
+          className={`relative group transition duration-500 font-bold ${
+            pathname === "/feed"
+              ? "text-[#f09433]"
+              : "text-gray-200 hover:text-[#dc2743]"
+          }`}
+        >
+          FEED
+          <span
+            className={`absolute -bottom-0.5 left-0 h-0.5 bg-[#bc1888] transition-all duration-500 ${
+              pathname === "/feed" ? "w-full" : "w-0 group-hover:w-full"
+            }`}
+          ></span>
+        </Link>
+
+        <Link
+          href="/favorites"
+          className={`relative group transitio duration-500 font-bold ${
+            pathname === "/favorites"
+              ? "text-[#f09433]"
+              : "text-gray-200 hover:text-[#dc2743]"
+          }`}
+        >
+          FAVORITOS
+          <span
+            className={`absolute -bottom-0.5 left-0 h-0.5 bg-[#bc1888] transition-all duration-500 ${
+              pathname === "/favorites" ? "w-full" : "w-0 group-hover:w-full"
+            }`}
+          ></span>
+        </Link>
+
+        {isLoggedIn && (
+          <button
+            onClick={handleLogout}
+            className="relative group text-gray-200 hover:text-[#dc2743] transition duration-700 cursor-pointer font-bold"
           >
-            InstaPocket
-          </TypingAnimation>
-        </h1>
-
-        <nav className="flex items-center gap-4 text-sm font-medium px-2 space-x-8">
-            <a>
-            FEED
-            </a>
-
-            <a>
-            FAVORITOS
-            </a>
-            
-            <a>
             SAIR
-            </a>
-        </nav>
-      </header>
-    );
+            <span
+              className={`absolute -bottom-0.5 left-0 h-0.5 bg-[#bc1888] transition-all duration-700 w-0 group-hover:w-full`}
+            ></span>
+          </button>
+        )}
+      </nav>
+    </header>
+  );
 }
