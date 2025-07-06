b0539c263296b1c6cf9b97767c122678c2f669d2
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-07-06 18:12:09 -0300

    feat: including SearchBar

--- mini-instagram/src/app/feed/page.tsx
+++ mini-instagram/src/app/feed/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 import Header from "@/components/Header";
+import SearchBar from "@/components/SearchBar";
 
 export default function Feed() {
     return (
         <main className="w-full max-w-6xl px-4 py-6 mx-auto overflow-x-hidden">
             <Header />
+            <SearchBar />
         </main>
     )
 }
