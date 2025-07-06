a032940ff028c9537aa63f5da853a88dbd6f604f
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-07-06 15:14:17 -0300

    feat: implement login form with state management, validation and error display

--- mini-instagram/src/app/login/page.tsx
+++ mini-instagram/src/app/login/page.tsx
@@ -3,12 +3,33 @@
 import { TypingAnimation } from "@/components/magicui/typing-animation";
 import { cn } from "@/lib/utils";
 import { Lobster } from "next/font/google";
+import { useState } from "react";
 // import { useRouter } from "next/navigation"
 
 const lobster = Lobster({ weight: "400", subsets: ["latin"] });
 
 export default function LoginPage() {
   // const router = useRouter()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [erro, setErro] = useState("");
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    setErro("");
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      setErro("Digite um email válido!");
+      return;
+    }
+
+    if (password.length < 6) {
+      setErro("A senha deve ter pelo menos 6 caracteres!");
+      return;
+    }
+  };
 
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-foreground text-background px-4">
@@ -26,7 +47,10 @@ export default function LoginPage() {
         </TypingAnimation>
       </h1>
 
-      <form className="bg-gray-900 p-8 rounded shadow-md w-full max-w-md">
+      <form
+        onSubmit={handleLogin}
+        className="bg-gray-900 p-8 rounded-md shadow-md w-full max-w-md"
+      >
         <h2
           className={cn(
             "text-4xl text-center font-bold leading-[5rem] tracking-[-0.02em]",
@@ -37,13 +61,44 @@ export default function LoginPage() {
           Login
         </h2>
 
-        <label htmlFor="">Email:</label>
-        <input type="email" />
+        {erro && (
+          <div className="absolute z-50 top-8 bg-red-500 text-white px-6 py-4 rounded-md shadow-lg max-w-sm w-full animate-fade-in">
+            <p className="text-md text-center font-bold">{erro}</p>
+            <button
+              onClick={() => setErro("")}
+              className="mt-3 block mx-auto bg-white text-red-600 px-4 py-1 rounded hover:bg-gray-100 font-bold"
+            >
+              OK
+            </button>
+          </div>
+        )}
 
-        <label htmlFor="">Senha:</label>
-        <input type="password" name="" id="" />
+        <label className="block mb-2 text-sm font-medium">Email:</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          className="w-full px-3 py-2 mb-4 rounded bg-gray-800 border border-gray-600 focus:outline-none focus:border-[#bc1888] transition"
+          placeholder="seuemail@exemplo.com"
+        />
 
-        <button>ENTRAR</button>
+        <label className="block mb-2 text-sm font-medium">Senha:</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          className="w-full px-3 py-2 mb-6 rounded bg-gray-800 border border-gray-600 focus:outline-none focus:border-[#bc1888] transition"
+          placeholder="Mínimo 6 caracteres"
+        />
+
+        <button
+          type="submit"
+          className="w-full py-2 rounded text-white font-semibold transition duration-300 cursor-pointer 
+             bg-gradient-to-r from-[#f09433] via-[#dc2743] to-[#bc1888] 
+             bg-[length:200%_200%] animate-gradient"
+        >
+          ENTRAR
+        </button>
       </form>
     </main>
   );
