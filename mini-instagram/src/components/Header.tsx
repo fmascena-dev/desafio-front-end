75f7fc68900e1f6de18000a2c66a39c24dac709b
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-07-07 00:01:04 -0300

    refactor(header): use reusable Logo component

--- mini-instagram/src/components/Header.tsx
+++ mini-instagram/src/components/Header.tsx
@@ -1,12 +1,9 @@
 "use client";
-import { Lobster } from "next/font/google";
-import { TypingAnimation } from "./magicui/typing-animation";
-import { cn } from "@/lib/utils";
+
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-
-const lobster = Lobster({ weight: "400", subsets: ["latin"] });
+import Logo from "./Logo";
 
 export default function Header() {
   const pathname = usePathname();
@@ -25,19 +22,7 @@ export default function Header() {
 
   return (
     <header className="flex flex-col justify-between items-center mb-6 md:flex-row lg:flex-row">
-      <h1
-        className={`${lobster.className} text-2xl font-bold text-[#00ffd5] h-20`}
-      >
-        <TypingAnimation
-          className={cn(
-            "text-4xl font-bold leading-[5rem] tracking-[-0.02em]",
-            "bg-gradient-to-r from-[#f09433] via-[#dc2743] to-[#bc1888]",
-            "bg-[length:200%_200%] bg-clip-text text-transparent animate-gradient"
-          )}
-        >
-          InstaPocket
-        </TypingAnimation>
-      </h1>
+      <Logo />
 
       <nav className="flex items-center gap-4 text-sm font-medium px-2 space-x-8">
         <Link
