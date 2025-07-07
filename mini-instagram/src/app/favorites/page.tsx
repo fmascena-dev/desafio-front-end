3af78a9cc27e0e0225b7f7a12d65cd5e35584f54
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-07-07 13:52:27 -0300

    feat: implement 'clear all favorites' functionality with confirmation modal

--- mini-instagram/src/app/favorites/page.tsx
+++ mini-instagram/src/app/favorites/page.tsx
@@ -4,9 +4,24 @@ import Header from "@/components/Header";
 import PhotoCard from "@/components/PhotoCard";
 import { cn } from "@/lib/utils";
 import { useFavorites } from "@/store/useFavorites";
+import { useState } from "react";
 
 export default function FavoritesPage() {
-  const { favorites } = useFavorites();
+  const { favorites, clearFavorites } = useFavorites();
+  const [showConfirm, setShowConfirm] = useState(false);
+
+  const handleDeleteClick = () => {
+    setShowConfirm(true);
+  };
+
+  const handleConfirmYes = () => {
+    clearFavorites();
+    setShowConfirm(false);
+  };
+
+  const handleConfirmNo = () => {
+    setShowConfirm(false);
+  };
 
   return (
     <main className="px-4 py-6 max-w-6xl mx-auto">
@@ -26,10 +41,45 @@ export default function FavoritesPage() {
           Nenhuma imagem favoritada
         </p>
       ) : (
-        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-6">
-          {favorites.map((photo) => (
-            <PhotoCard key={photo.id} photo={photo} />
-          ))}
+        <>
+          <div className="flex justify-end mt-4">
+            <button
+              onClick={handleDeleteClick}
+              className="bg-red-600 hover:bg-red-800 text-white px-4 py-2 rounded-md font-bold transition duration-300 cursor-pointer"
+            >
+              Excluir todos
+            </button>
+          </div>
+
+          <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-6">
+            {favorites.map((photo) => (
+              <PhotoCard key={photo.id} photo={photo} />
+            ))}
+          </div>
+        </>
+      )}
+
+      {showConfirm && (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center z-50">
+          <div className="bg-white p-6 rounded-lg shadow-xl w-full max-w-sm text-center">
+            <h2 className="text-xl font-bold mb-4">
+              Tem certeza que deseja excluir todos os favoritos?
+            </h2>
+            <div className="flex justify-center gap-4">
+              <button
+                onClick={handleConfirmYes}
+                className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-md font-bold"
+              >
+                Sim
+              </button>
+              <button
+                onClick={handleConfirmNo}
+                className="bg-gray-300 hover:bg-gray-400 text-gray-900 px-4 py-2 rounded-md font-bold"
+              >
+                Não
+              </button>
+            </div>
+          </div>
         </div>
       )}
     </main>
